fb9b5fa0839e30da2a83938d6c8c2298e88c7492
JackRichards321 <sjackrichards88@gmail.com>
2022-07-12 00:56:44 -0400

    second commit, home progress. typewriter

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,24 +1,9 @@
 import { Component } from '@angular/core';
-import {
-  trigger, state, style, animate, transition, query, group
-} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  animations: [
-    trigger('fade', [
-      transition(':enter', [
-        style({ opacity: 0 }),
-        animate(1000, style({ opacity: 1 }))
-      ]),
-      transition(':leave', [
-        style({ opacity: 1 }),
-        animate(1000, style({ opacity: 0 }))
-      ])
-    ])
-  ]
 })
 export class AppComponent {
   title = 'The Shore Foundation';
--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+// @ts-ignore
+import Typewriter from 't-writer.js';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +12,17 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const target = document.querySelector('.tw');
+    const writer = new Typewriter(target, {
+      loop: false,
+      typeColor: 'lightgray'
+    })
+    
+    writer
+      .type('Bridging the digital divide and improving lives through access to technology.')
+      .rest(500)
+      .start()
+            
   }
 
 }
